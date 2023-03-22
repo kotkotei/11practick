f01ea8b5f2f50f989b4da90f3473fa35869f5cdb
kotkotei <kotkotei@yandex.ru>
2023-03-22 13:21:32 +0700

    lol

---
+++ Pr11Sistem/Pr11Sistem/ScreenShot.Designer.cs
@@ -0,0 +1,214 @@
+﻿
+namespace Pr11Sistem
+{
+    partial class ScreenShot
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(536, 275);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(574, 342);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(213, 54);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Сохранить в png";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(575, 282);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(213, 54);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Сохранить в jpg";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 282);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 54);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Старт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(114, 282);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(96, 54);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Стоп";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(612, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 17);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Выбор частоты ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 361);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "label2";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(216, 282);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(200, 41);
+            this.button5.TabIndex = 13;
+            this.button5.Text = "Выбор папки сохранения";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(631, 40);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(77, 45);
+            this.button6.TabIndex = 24;
+            this.button6.Text = "30 сек";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click_1);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(714, 40);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(77, 45);
+            this.button7.TabIndex = 25;
+            this.button7.Text = "60 сек";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(546, 40);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(77, 45);
+            this.button8.TabIndex = 26;
+            this.button8.Text = "10 сек";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(631, 91);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(77, 45);
+            this.button9.TabIndex = 27;
+            this.button9.Text = "1 сек";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // ScreenShot
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "ScreenShot";
+            this.Text = "ScreenShot";
+            this.Load += new System.EventHandler(this.ScreenShot_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
+        public System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+    }
+}
---
+++ Pr11Sistem/Pr11Sistem/ScreenShot.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Timers;
+using System.IO;
+
+namespace Pr11Sistem
+{
+    public partial class ScreenShot : Form
+    {
+        public static Bitmap BM = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+        int t = 0;
+        public ScreenShot()
+        {
+            InitializeComponent();
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.Image = Form1.BM;
+            
+        }
+       
+
+        private void ScreenShot_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //SaveFileDialog SFD = new SaveFileDialog();
+            //SFD.Filter = "JPEG|*.jpg";
+            //if (SFD.ShowDialog() == DialogResult.OK)
+            //{
+            //    Form1.BM.Save(SFD.FileName);
+            //}
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //SaveFileDialog SFD = new SaveFileDialog();
+            //SFD.Filter = "PNG|*.png";
+            //if (SFD.ShowDialog() == DialogResult.OK)
+            //{
+            //    Form1.BM.Save(SFD.FileName);
+            //}
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start(); 
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            Graphics GH = Graphics.FromImage(BM as Image);
+            GH.CopyFromScreen(0, 0, 0, 0, BM.Size);
+            t++;
+            BM.Save(label2.Text+"\\"+t.ToString()+".jpg");
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    string[] files = Directory.GetFiles(fbd.SelectedPath);
+                    label2.Text = Convert.ToString(fbd.SelectedPath);
+
+                    //System.Windows.Forms.MessageBox.Show("Files found: " + files.Length.ToString(), "Message");
+                }
+            }
+        }
+
+        private void button6_Click_1(object sender, EventArgs e)
+        {
+            timer1.Interval = 30000;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            timer1.Interval = 60000;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            timer1.Interval = 10000;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            timer1.Interval = 1000;
+        }
+    }
+}
